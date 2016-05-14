917471d33165a6f78086e637d60afedeb0929ecf
Alec Rust <me@alecrust.com>
2016-05-14 17:41:46 +0100

    Compile CSS with PostCSS/Autoprefixer

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,15 @@ import browserSync from 'browser-sync';
 
 const $ = gulpLoadPlugins();
 
+gulp.task('css', () => {
+  return gulp.src('src/css/style.css')
+    .pipe($.postcss([
+      require('autoprefixer')
+    ]))
+    .pipe($.rename('style.css'))
+    .pipe(gulp.dest('.'));
+});
+
 gulp.task('deploy', () => {
   return gulp.src('./public/**/*')
     .pipe($.ghPages({
