6d68fe6b6db616f7bc0709983e45448211e3df68
Alec Rust <me@alecrust.com>
2016-06-26 21:40:34 +0100

    Switch dates to ISO 8601 syntax
    Fixes tests by using ISO format in `resume.json` and converting to a friendly format with a Handlebars helper using date-fns.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var fs = require('fs');
 var Handlebars = require('handlebars');
+var formatDate = require('date-fns/format')
+
+Handlebars.registerHelper('friendlyDate', function(datestamp) {
+    var date = new Date(datestamp);
+    return formatDate(date, 'Do MMM YYYY');
+});
 
 function render(resume) {
   var tpl = fs.readFileSync(__dirname + '/resume.hbs', 'utf-8');
