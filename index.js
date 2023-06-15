220c95fa09fd9bc9af2eadec521174d07200a83f
Alec Rust <me@alecrust.com>
2023-06-15 18:18:53 +0100

    Upgrade date-fns to v2

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const fs = require('fs');
+const path = require('path');
 const Handlebars = require('handlebars');
-const parse = require('date-fns/parse');
+const parseISO = require('date-fns/parseISO');
 const formatDate = require('date-fns/format');
 
 Handlebars.registerHelper('friendlyDate', function(datestamp) {
-  const date = parse(datestamp);
-  return datestamp ? formatDate(date, 'Do MMM YYYY') : 'present';
+  if (datestamp) {
+    const date = parseISO(datestamp);
+    return formatDate(date, 'do MMM yyyy');
+  }
+  return 'present';
 });
 
 function render(resume) {
-  const tpl = fs.readFileSync(__dirname + '/src/resume.hbs', 'utf-8');
-  return Handlebars.compile(tpl)({
-    resume: resume
-  });
+  const tpl = fs.readFileSync(
+    path.join(__dirname, 'src', 'resume.hbs'),
+    'utf-8'
+  );
+  const compile = Handlebars.compile(tpl);
+  return compile({ resume });
 }
 
 module.exports = {
-  render: render
+  render
 };
